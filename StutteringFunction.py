95b4735cef1d695db583cb381e28c359a4f79af6
Danny-Votez <77758884+danny-votez@users.noreply.github.com>
2021-10-11 11:53:23 +0300

    Update StutteringFunction.py

--- StutteringFunction.py
+++ StutteringFunction.py
@@ -1,7 +1,8 @@
 # Writing a function that stutters a word as if someone is struggling to read it
-# For example, with any word entered, there is repeating the first tow letters twice, with an ellipsis
+# For example, with any word entered, there is repeating the first two letters twice, with an ellipsis
 #  The final word is then read out
 # This program relies on user input
+
 stutterWord = input("Enter the word to stutter: ")
 print(stutterWord[:2], ".......", stutterWord[:2], "........", stutterWord)
 print(stutterWord,"\n")
